a04f42036f29601e04e753bf8a6eaff427a31fb8
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2024-04-23 15:03:11 +0700

    gifbg draft1

--- src/pane/RootPane.java
+++ src/pane/RootPane.java
@@ -4,10 +4,7 @@ import GUI.spacePane;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -18,19 +15,53 @@ public class RootPane extends StackPane {
     private static RootPane instance;
     public PaintPane paintPane;
 
-    private RootPane() {
-        paintPane = new PaintPane();
-        Image image = new Image("background.png");
-        BackgroundImage backgroundImage = new BackgroundImage(image, null, null, null, null);
-        Background background = new Background(backgroundImage);
-        this.setBackground(background);
-        this.setAlignment(Pos.TOP_CENTER);
-        this.getChildren().add(paintPane);
-        paintPane.drawTurn();
+//    private RootPane() {
+//        paintPane = new PaintPane();
+//        Image image = new Image("gif.gif");
+//
+//        BackgroundImage backgroundImage = new BackgroundImage(image, null, null, null, null);
+//        Background background = new Background(backgroundImage);
+//        this.setBackground(background);
+//        this.setAlignment(Pos.TOP_CENTER);
+//        this.getChildren().add(paintPane);
+//        paintPane.drawTurn();
+//
+//        spacePane spacePane = new spacePane();
+//        this.getChildren().add(spacePane);
+//    }
+private RootPane() {
+    paintPane = new PaintPane();
+    Image image = new Image("gif2.gif");
 
-        spacePane spacePane = new spacePane();
-        this.getChildren().add(spacePane);
-    }
+    // Create a BackgroundSize object that will fill the area
+    BackgroundSize backgroundSize = new BackgroundSize(
+            100, // Width is 100% of area
+            100, // Height is 100% of area
+            true, // Width as percentage
+            true, // Height as percentage
+            false, // Do not preserve image's width/height ratio
+            true  // Cover the available area
+    );
+
+    // Configure the BackgroundImage
+    BackgroundImage backgroundImage = new BackgroundImage(
+            image,
+            BackgroundRepeat.NO_REPEAT, // Image will not repeat
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundPosition.CENTER, // Image centered in the background
+            backgroundSize              // Use the custom BackgroundSize
+    );
+
+    Background background = new Background(backgroundImage);
+    this.setBackground(background);
+    this.setAlignment(Pos.TOP_CENTER);
+    this.getChildren().add(paintPane);
+    paintPane.drawTurn();
+
+    spacePane spacePane = new spacePane();
+    this.getChildren().add(spacePane);
+}
+
 
     public static RootPane getRootPane() {
         if (instance == null)
