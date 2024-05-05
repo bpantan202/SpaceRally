06086c225e7caf7174000d4cea3897bbb684b012
Pannavich Tanyavichitkul <pannavich.tan@gmail.com>
2024-05-05 23:39:16 +0700

    final

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,72 +1,115 @@
 package main;
 
+import GUI.*;
+import Sound.Sound;
 import game.GameController;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
-import GUI.RootPane;
 
-//public class Main {
-//    public static void main(String[] args)  {
-//        GameController gameInstance = GameController.getInstance();
-//        gameInstance.getGameMap().printHoldMap();
-//    }
+import java.util.ArrayList;
 
 
-    public class Main extends Application {
+public class Main extends Application {
+    private Sound backgroundMusic;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        RootPane rootPane = RootPane.getRootPane();
+        Scene scene = new Scene(rootPane,1600,900);
 
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-//             TODO Auto-generated method stub
-//            HBox root = new HBox();
-//            root.setPadding(new Insets(10));
-//            root.setSpacing(10);
-//            root.setPrefHeight(900);
-//
-//            root.setPrefWidth(1600);
-//            //GameLogic.getInstance();
-//            spacePane spacePane = new spacePane();
-//            //ControlPane controlPane = new ControlPane(ticTacToePane);
-//
-//            //GameLogic.getInstance().setControlPane(controlPane);
-//
-//
-//            root.getChildren().add(spacePane);
-//            //root.getChildren().add(controlPane);
+        PaintPane paintPane = PaintPane.getInstance();
+
+        GraphicsContext gc = paintPane.getGraphicsContext2D();
+        RenderableHolder renderableHolder = RenderableHolder.getInstance();
 
 
-            RootPane rootPane = RootPane.getRootPane();
-            Scene scene = new Scene(rootPane,1600,900);
+        DicePane dicePane = DicePane.getInstance();
+        rootPane.getChildren().add(dicePane);
+        Thread.sleep(4000);
 
-            GameController.getInstance();
-//            rootPane.setGameController() = GameController.getInstance();
-//            SpacePane spacePane = new SpacePane();
-//            ControlPane controlPane = new ControlPane(ticTacToePane);
 
-//            GameController.getInstance().setControlPane(controlPane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("SpaceRally");
+        primaryStage.show();
 
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("SpaceRally");
-            primaryStage.setFullScreen(true);
-            primaryStage.show();
-            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-                @Override
-                public void handle(WindowEvent e) {
-                    Platform.exit();
-                    System.exit(0);
+
+        backgroundMusic = new Sound("backgroundMusic.mp3");
+        backgroundMusic.loop();
+
+        GameController gameController = GameController.getInstance();
+
+        PlayerStatusPane playerStatusPane = new PlayerStatusPane(gameController.getPlayers(),gc);
+        rootPane.getChildren().add(playerStatusPane);
+
+        TurnPane turnPane = new TurnPane(gc);
+        rootPane.getChildren().add(turnPane);
+
+        ControlPane controlPane = new ControlPane();
+        rootPane.getChildren().add(controlPane);
+
+
+        ArrayList<Boolean> valid =  GameController.getInstance().askValid();
+        controlPane.setValidButton(valid.get(0), valid.get(1), valid.get(2), valid.get(3));
+        paintPane.drawAll(renderableHolder.getObjects());
+        controlPane.updateGameText(GameController.getInstance().getPlayerDisplay());
+
+        MenuPane menuPane = new MenuPane();
+        rootPane.getChildren().add(menuPane);
+
+        AnimationTimer animationTimer = new AnimationTimer() {
+
+            private void setButton() {
+                ArrayList<Boolean> valid = GameController.getInstance().askValid();
+                while (!(valid.get(0) || valid.get(1) || valid.get(2) || valid.get(3))){
+                    GameController.getInstance().nextPlayer();
+                    valid = GameController.getInstance().askValid();
                 }
-            });
-//            GameController gameInstance = GameController.getInstance();
-//            gameInstance.getGameMap().printHoldMap();
-        }
+                controlPane.setValidButton(valid.get(0), valid.get(1), valid.get(2), valid.get(3));
+                controlPane.updateGameText(GameController.getInstance().getPlayerDisplay());
+            }
 
 
-        public static void main(String[] args) {
-            launch(args);
-        }
+            @Override
+            public void handle(long l) {
+                if(controlPane.getGameText() == "Left"){
+                    gameController.playTurnEach(1);
+                    setButton();
+
+                    paintPane.drawAll(renderableHolder.getObjects());
+
+                }
+                else if(controlPane.getGameText() == "Right"){
+                    gameController.playTurnEach(2);
+                    setButton();
+
+                    paintPane.drawAll(renderableHolder.getObjects());
+
+                }
+                if(controlPane.getGameText() == "Up"){
+                    gameController.playTurnEach(3);
+                    setButton();
+
+                    paintPane.drawAll(renderableHolder.getObjects());
+
+                } else if (controlPane.getGameText() == "Down") {
+                    gameController.playTurnEach(4);
+                    setButton();
+
+                    paintPane.drawAll(renderableHolder.getObjects());
+                }
+
+            }
+        };
+        animationTimer.start();
+
+
+
     }
 
-//}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/usage/Direction.java
+++
@@ -1,5 +0,0 @@
-//package usage;
-//
-//public enum Direction {
-//    UP,DOWN,LEFT,RIGHT
-//}
